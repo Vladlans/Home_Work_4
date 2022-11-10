a20ed6b5768636fa28973a8ab91ad1aae25f729d
Vlad <blackmor143@yandex.ru>
2022-11-10 03:12:58 +0300

    Выполнил 3 задание

---
+++ Zadanie_3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.(в примере показаны два разных массива, создаем именно для 8 элементов)
+void GetArray(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 20);
+        Console.Write($" {arr[i]};");
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($"[");
+    int size = arr.Length;
+    for (int i = 0; i < size - 1; i++)
+    {
+        Console.Write($"{arr[i]}; ");
+    }
+    Console.Write($"{arr[size - 1]}]");
+    Console.WriteLine();
+}
+
+int[] arr = new int[8];
+GetArray(arr);
+PrintArray(arr);
