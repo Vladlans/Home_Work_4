a9f2a947bffcc4ce9252c9582685a35791d22821
Vlad <blackmor143@yandex.ru>
2022-11-10 03:12:21 +0300

    Выполнил 1 задание

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int GetPowNums(int number, int power)
+{
+    int powerResult = 1;
+    for (int i = 1; i <= power; i++)
+    {
+        powerResult *= number;
+    }
+    return powerResult;
+}
+Console.WriteLine();
+
+Console.WriteLine("Введите натуральное число A: ");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B: ");
+int numB = int.Parse(Console.ReadLine());
+
+int rezult = GetPowNums(numA, numB);
+
+Console.WriteLine($"Число {numA} в степени {numB} = {rezult}.");
