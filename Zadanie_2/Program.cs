cf038be9b8108b3647ad026b248ccbebd8a6b1db
Vlad <blackmor143@yandex.ru>
2022-11-10 03:12:38 +0300

    Выполнил 2 задание

---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+string InputNumber()
+{
+    Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+if (int.Parse(num[0].ToString()) == 0)
+Console.WriteLine($"Неподходящее значение!");
+return num;
+}
+
+int SumDigitalsOfNumber(string num)
+{
+   int sum = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        sum = sum + int.Parse(num[i].ToString());
+    }
+    Console.WriteLine($"Сумма цифр в чиселе {num} = {sum}"); 
+    return sum;
+}
+SumDigitalsOfNumber(InputNumber());
